7b6e97427f8846563d86582347465533bdc1a381
Mateusz Woźniak <matisiek11@gmail.com>
2023-09-03 13:48:36 +0200

    Added reports

--- go.mod
+++ go.mod
@@ -3,21 +3,30 @@ module github.com/matisiekpl/pansa-plan
 go 1.20
 
 require (
+	github.com/anaskhan96/soup v1.2.5
 	github.com/desertbit/fillpdf v0.0.2
+	github.com/flopp/go-staticmaps v0.0.0-20220221183018-c226716bec53
 	github.com/google/uuid v1.3.0
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
-	github.com/anaskhan96/soup v1.2.5 // indirect
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.1 // indirect
+	github.com/aymerick/raymond v2.0.2+incompatible // indirect
+	github.com/flopp/go-coordsparser v0.0.0-20201115094714-8baaeb7062d5 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/tkrajina/gpxgo v1.1.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
---
+++ report.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"encoding/base64"
+	"fmt"
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+	"github.com/aymerick/raymond"
+	sm "github.com/flopp/go-staticmaps"
+	"github.com/golang/geo/s2"
+	"github.com/labstack/echo/v4"
+	"image"
+	"image/color"
+	"image/png"
+	"net/http"
+	"strings"
+)
+
+//go:embed report.html
+var reportTemplate string
+
+func generateReport(c echo.Context) error {
+	var plan Plan
+	err := c.Bind(&plan)
+	if err != nil {
+		return err
+	}
+	mapImage, err := createMapImage(plan)
+	var mapImageBuffer bytes.Buffer
+	err = png.Encode(&mapImageBuffer, mapImage)
+	if err != nil {
+		return err
+	}
+	encodedMapImage := "data:image/png;base64," + base64.StdEncoding.EncodeToString(mapImageBuffer.Bytes())
+	result, err := raymond.Render(reportTemplate, map[string]interface{}{
+		"plan": plan,
+		"map":  encodedMapImage,
+	})
+	pdf, err := wkhtmltopdf.NewPDFGenerator()
+	if err != nil {
+		return err
+	}
+	pdf.AddPage(wkhtmltopdf.NewPageReader(strings.NewReader(result)))
+	err = pdf.Create()
+	return c.Blob(http.StatusOK, "application/pdf", pdf.Bytes())
+}
+
+type Plan struct {
+	Name      string     `json:"name"`
+	Distance  string     `json:"distance"`
+	Duration  string     `json:"duration"`
+	Waypoints []Waypoint `json:"waypoints"`
+}
+
+type Waypoint struct {
+	Name            string  `json:"name"`
+	Latitude        float64 `json:"latitude"`
+	Longitude       float64 `json:"longitude"`
+	MagneticTrack   string  `json:"magneticTrack"`
+	MagneticHeading string  `json:"magneticHeading"`
+	Wind            string  `json:"wind"`
+	Distance        string  `json:"distance"`
+	Duration        string  `json:"duration"`
+}
+
+func createMapImage(plan Plan) (image.Image, error) {
+	ctx := sm.NewContext()
+	for i, waypoint := range plan.Waypoints {
+		marker := sm.NewMarker(
+			s2.LatLngFromDegrees(waypoint.Latitude, waypoint.Longitude),
+			color.RGBA{R: 0xff, A: 0xff},
+			16.0,
+		)
+		marker.Label = fmt.Sprintf("%d. %s", i+1, waypoint.Name)
+		marker.LabelColor = color.Black
+		marker.LabelYOffset = -1
+		ctx.AddObject(marker)
+	}
+	for i := 0; i < len(plan.Waypoints)-1; i++ {
+		ctx.AddObject(
+			sm.NewPath(
+				[]s2.LatLng{
+					s2.LatLngFromDegrees(plan.Waypoints[i].Latitude, plan.Waypoints[i].Longitude),
+					s2.LatLngFromDegrees(plan.Waypoints[i+1].Latitude, plan.Waypoints[i+1].Longitude),
+				},
+				color.RGBA{R: 0xff, A: 0xff},
+				3,
+			),
+		)
+	}
+	return ctx.Render()
+}
