04ddee12ad8fdf780b7cdff1d6a1a665e92b85c3
Mateusz Woźniak <matisiek11@gmail.com>
2025-05-07 11:14:53 +0200

    feat: new eaip parser

--- go.mod
+++ go.mod
@@ -5,20 +5,20 @@ go 1.21
 toolchain go1.22.2
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.1
-	github.com/anaskhan96/soup v1.2.5
 	github.com/aws/aws-lambda-go v1.41.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/aymerick/raymond v2.0.2+incompatible
 	github.com/flopp/go-staticmaps v0.0.0-20220221183018-c226716bec53
 	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551
+	github.com/kaptinlin/jsonrepair v0.1.1
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/matisiekpl/notam v0.0.0-20230903190302-ed364446b8e7
 	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/flopp/go-coordsparser v0.0.0-20201115094714-8baaeb7062d5 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
--- internal/repository/publication.go
+++ internal/repository/publication.go
@@ -1,13 +1,63 @@
 package repository
 
 import (
-	"github.com/anaskhan96/soup"
+	"encoding/json"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/kaptinlin/jsonrepair"
 	"github.com/matisiekpl/pansa-plan/internal/model"
 	"github.com/sirupsen/logrus"
-	"os"
+	"io"
+	"net/http"
 	"strings"
 	"time"
 )
+
+type PansaLanguage struct {
+	ID           int    `json:"id"`
+	Code         string `json:"code"`
+	Flag         string `json:"flag"`
+	History      string `json:"history"`
+	HistoryLabel string `json:"historyLabel"`
+	Help         string `json:"help"`
+	HelpLabel    string `json:"helpLabel"`
+	Cover        string `json:"cover"`
+	CoverText    string `json:"coverText"`
+	Name         string `json:"name"`
+}
+
+type PansaCommands struct {
+	Title     string          `json:"title"`
+	Languages []PansaLanguage `json:"languages"`
+}
+
+type PansaMenuItem struct {
+	Parent    string          `json:"parent"`
+	ID        string          `json:"id"`
+	Href      string          `json:"href"`
+	Title     string          `json:"title"`
+	Depth     int             `json:"depth"`
+	Level     int             `json:"level"`
+	Collapsed bool            `json:"collapsed"`
+	Children  []PansaMenuItem `json:"children"`
+}
+
+type PansaTabContent struct {
+	Title string          `json:"title"`
+	Menu  []PansaMenuItem `json:"menu"`
+}
+
+type PansaTab struct {
+	ID       int                        `json:"id"`
+	Title    string                     `json:"title"`
+	Contents map[string]PansaTabContent `json:"contents"`
+}
+
+type PansaTabData struct {
+	Generated string        `json:"generated"`
+	Commands  PansaCommands `json:"commands"`
+	Tabs      []PansaTab    `json:"tabs"`
+}
 
 type PublicationRepository interface {
 	Index() []model.Publication
@@ -23,68 +73,243 @@ func newPublicationRepository() PublicationRepository {
 	}
 }
 
-func (publicationRepository) query(source string) []model.Publication {
-	var publications []model.Publication
-	resp, err := soup.Get(source)
-	if err != nil {
-		os.Exit(1)
-	}
-	doc := soup.HTMLParse(resp)
-	elements := doc.FindAll("details")
-	for _, element := range elements {
-		if element.Attrs()["class"] == "sub-details" {
-			label := element.Children()[0].Text()
-			icao := strings.TrimSpace(strings.ReplaceAll(label, "VFR", ""))
-			for _, link := range element.Children()[1].FindAll("a") {
-				url := strings.ReplaceAll(strings.ReplaceAll(link.Attrs()["href"], " ", ""), "	", "")
-				name := strings.TrimSpace(strings.ReplaceAll(link.Text(), "\t", ""))
-				publications = append(publications, model.Publication{
-					Icao: icao,
-					Name: name,
-					Link: url,
-					Type: "AD",
-				})
-			}
-		}
+func (r *publicationRepository) Index() []model.Publication {
+	cacheKey := time.Now().Format(time.DateOnly)
 
-		for _, link := range element.FindAll("a") {
-			url := strings.ReplaceAll(strings.ReplaceAll(link.Attrs()["href"], " ", ""), "	", "")
-			name := strings.TrimSpace(strings.ReplaceAll(link.Text(), "\t", ""))
-			if strings.Contains(url, "_Sup_") {
-				publications = append(publications, model.Publication{
-					Icao: "",
-					Name: name,
-					Link: url,
-					Type: "SUP",
-				})
-			}
-			if strings.Contains(url, "_GEN_") {
-				publications = append(publications, model.Publication{
-					Icao: "",
-					Name: name,
-					Link: url,
-					Type: "GEN",
-				})
-			}
-			if strings.Contains(url, "_ENR_") {
-				publications = append(publications, model.Publication{
-					Icao: "",
-					Name: name,
-					Link: url,
-					Type: "ENR",
-				})
-			}
-		}
+	if cached, exists := r.cache[cacheKey]; exists {
+		logrus.Infof("Found cached %d publications for %s", len(cached), cacheKey)
+		return cached
 	}
+
+	link, err := r.findEAIPLink()
+	if err != nil {
+		logrus.Error(err)
+		return nil
+	}
+
+	logrus.Infof("Found EAIP link: %s", link)
+
+	amendmentLink, err := r.findCurrentAmendmentLink(link)
+	if err != nil {
+		logrus.Error(err)
+		return nil
+	}
+
+	logrus.Infof("Found amendment link: %s", amendmentLink)
+
+	tabs, err := r.extractTabs(amendmentLink)
+	if err != nil {
+		logrus.Error(err)
+		return nil
+	}
+
+	logrus.Infof("Extracted %d tabs", len(tabs.Tabs))
+
+	publications := r.combine(tabs, amendmentLink)
+	publications = r.filterDuplicates(publications)
+
+	logrus.Infof("Found %d publications", len(publications))
+
+	r.cache[cacheKey] = publications
 	return publications
 }
 
-func (p publicationRepository) Index() []model.Publication {
-	p.query("https://www.ais.pansa.pl/publikacje/aip-vfr/")
-	if val, ok := p.cache[time.Now().Format(time.DateOnly)]; ok {
-		return val
+func (r *publicationRepository) findEAIPLink() (string, error) {
+	resp, err := http.Get("https://www.ais.pansa.pl/publikacje/aip-vfr/")
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch URL: %v", err)
 	}
-	p.cache[time.Now().Format(time.DateOnly)] = append(p.query("https://www.ais.pansa.pl/publikacje/aip-vfr/"), p.query("https://www.ais.pansa.pl/publikacje/aip-ifr/")...)
-	logrus.Infof("Downloaded %d AIPs", len(p.cache[time.Now().Format(time.DateOnly)]))
-	return p.cache[time.Now().Format(time.DateOnly)]
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("status code error: %d", resp.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse HTML: %v", err)
+	}
+
+	var eaipLink string
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		if href, exists := s.Attr("href"); exists {
+			if strings.Contains(strings.ToLower(href), "eaipvfr") {
+				eaipLink = href
+			}
+		}
+	})
+
+	if eaipLink == "" {
+		return "", fmt.Errorf("no eaip link found")
+	}
+
+	return eaipLink, nil
+}
+
+func (r *publicationRepository) findCurrentAmendmentLink(eaipLink string) (string, error) {
+	resp, err := http.Get(eaipLink)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch URL: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("status code error: %d", resp.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse HTML: %v", err)
+	}
+
+	var amendmentHref string
+	foundCurrent := false
+
+	doc.Find("h2").Each(func(i int, s *goquery.Selection) {
+		if s.Text() == "Obowiązująca Zmiana" {
+			foundCurrent = true
+			nextTable := s.NextUntil("h2").Filter("table").First()
+			nextTable.Find("a").First().Each(func(i int, a *goquery.Selection) {
+				if href, exists := a.Attr("href"); exists {
+					amendmentHref = href
+				}
+			})
+		}
+	})
+
+	if !foundCurrent || amendmentHref == "" {
+		return "", fmt.Errorf("no current amendment link found")
+	}
+
+	baseURL := eaipLink[:strings.LastIndex(eaipLink, "/")+1]
+	return baseURL + amendmentHref, nil
+}
+
+func (r *publicationRepository) extractTabs(amendmentLink string) (*PansaTabData, error) {
+	baseURL := amendmentLink[:strings.LastIndex(amendmentLink, "\\")+1]
+	datasourceURL := baseURL + "v2/js/datasource.js"
+
+	datasourceURL = strings.ReplaceAll(datasourceURL, "\\", "/")
+
+	logrus.Info("Fetching datasource.js from:", datasourceURL)
+
+	resp, err := http.Get(strings.ReplaceAll(datasourceURL, " ", "%20"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch datasource.js: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status code error: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	content := string(body)
+	content = strings.TrimPrefix(content, "const DATASOURCE = ")
+	content = strings.TrimSuffix(content, ";")
+	content = strings.ReplaceAll(content, "\t", "")
+
+	content, err = jsonrepair.JSONRepair(content)
+	if err != nil {
+		return nil, fmt.Errorf("failed to repair JSON: %v", err)
+	}
+
+	var data PansaTabData
+	if err := json.Unmarshal([]byte(content), &data); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %v", err)
+	}
+
+	return &data, nil
+}
+
+func (r *publicationRepository) getPDFLink(amendmentLink, href string) string {
+	href = strings.ReplaceAll(href, "-pl-PL", "")
+	href = strings.ReplaceAll(href, "-en-US", "")
+	href = strings.ReplaceAll(href, "-en-GB", "")
+	href = strings.ReplaceAll(href, ".html", ".pdf")
+	baseURL := amendmentLink[:strings.LastIndex(amendmentLink, "\\")+1]
+	link := baseURL + "documents/PDF/" + href + ".pdf"
+	link = strings.ReplaceAll(link, " ", "%20")
+	link = strings.ReplaceAll(link, "\\", "/")
+	link = strings.TrimSpace(link)
+	return link
+}
+
+func (r *publicationRepository) extractIcao(name string) string {
+	words := strings.Fields(name)
+	for _, word := range words {
+		if len(word) == 4 && strings.HasPrefix(word, "EP") {
+			return word
+		}
+	}
+	return ""
+}
+
+func (r *publicationRepository) combine(tabs *PansaTabData, amendmentLink string) []model.Publication {
+	var publications []model.Publication
+
+	for _, tab := range tabs.Tabs {
+		for _, content := range tab.Contents {
+			var processMenuItem func(item PansaMenuItem, itemType string)
+			processMenuItem = func(item PansaMenuItem, itemType string) {
+				if item.Href != "" {
+					pubType := itemType
+					if strings.Contains(item.Title, "AD") {
+						pubType = "AD"
+					} else if strings.Contains(item.Title, "ENR") {
+						pubType = "ENR"
+					} else if strings.Contains(item.Title, "GEN") {
+						pubType = "GEN"
+					}
+
+					link := item.Href
+					if itemType == "item" {
+						link = r.getPDFLink(amendmentLink, item.Href)
+					}
+
+					publications = append(publications, model.Publication{
+						Icao: r.extractIcao(item.Title),
+						Name: strings.TrimSpace(item.Title),
+						Link: link,
+						Type: pubType,
+					})
+				}
+
+				for _, child := range item.Children {
+					processMenuItem(child, "item")
+				}
+			}
+
+			for _, menu := range content.Menu {
+				processMenuItem(menu, "menu")
+			}
+		}
+	}
+
+	filteredPublications := make([]model.Publication, 0)
+	for _, publication := range publications {
+		if strings.HasPrefix(publication.Link, "https") {
+			filteredPublications = append(filteredPublications, publication)
+		}
+	}
+
+	return filteredPublications
+}
+
+func (r *publicationRepository) filterDuplicates(publications []model.Publication) []model.Publication {
+	seen := make(map[string]bool)
+	var unique []model.Publication
+
+	for _, pub := range publications {
+		if !seen[pub.Link] {
+			seen[pub.Link] = true
+			unique = append(unique, pub)
+		}
+	}
+
+	return unique
 }
